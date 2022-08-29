abb9ed107cd8f56a2efd5e0931d947ec7c0bc319
Luiz Sergio <lehmkala@gmail.com>
2022-08-29 12:47:53 -0300

    terceiro commit atualizando a questão 1071

--- 1140/1140.cpp
+++ 1140/1140.cpp
@@ -1,11 +1,50 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <cstdlib>
 
-int main(){
-	std::cout<<"Entre com frases"<<std::endl;
-	std::string str;
-	while(not std::getline(std::cin, str).eof()){
-		std::cout<<str;
-	}
-	return 0;
+bool split(std::string str, std::string del = " "){
+  
+  int start, end = -1*del.size(), code= 0, deltaCode=0;
+
+  do{  
+    
+    start = end + del.size();
+    end = str.find(del, start);
+    deltaCode = (str.substr(start, end-start)[0]); // código da primeira letra. Cada iteração vai assumir o código da nova palavra.
+    
+    if(code == 0){
+      code = deltaCode; //código da primeira letra, da primeira palavra, usado para fim de comparação com as próximas palavras.
+    }
+    
+    if(abs(code - deltaCode) != 32 && code - deltaCode != 0){ 
+      return false;
+      break;
+    }
+    
+  }while(end != -1);
+  
+  return true;
+  
+}
+ 
+int main() {
+ 
+  std::string str;
+  bool status;  
+  
+  while(getline(std::cin, str)){
+    if(str == "*"){
+      break;
+    }
+    
+    status = split(str," ");
+    
+    if(status){
+      std::cout<<"Y\n";
+    }else{
+      std::cout<<"N\n";
+    }
+  }
+  
+  return 0;
 }
